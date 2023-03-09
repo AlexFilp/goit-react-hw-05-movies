87c7c67d4dcbc4d8768ab95574364bbacef3f5cc
Oleksandr FIlippov <filya170895@gmail.com>
2023-03-09 22:39:01 +0100

    Update FilmsListItem.jsx

--- src/components/FilmsListItem/FilmsListItem.jsx
+++ src/components/FilmsListItem/FilmsListItem.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const FilmsListItem = ({ id, title }) => {
   return (
     <li>
-      <Link to={`${id}`}>{title}</Link>
+      <Link to={`/movies/${id}`}>{title}</Link>
     </li>
   );
 };
